10a4f8c156698b54952707bca3d6b4d63c8dcfb5
Alex <andreica.alexandru2000@gmail.com>
2020-05-24 15:51:46 +0300

    added patient entity and db configuration

---
+++ src/main/java/org/fasttrackit/massageapi/domain/Patient.java
@@ -0,0 +1,75 @@
+package org.fasttrackit.massageapi.domain;
+
+import org.springframework.lang.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+public class Patient {
+
+    @Id
+    private long id;
+    @NotNull
+    private String name;
+    @NotNull
+    private LocalDate date;
+    @NotNull
+    private long phone;
+    @NotNull
+    private String email;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date=" + date +
+                ", phone=" + phone +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
