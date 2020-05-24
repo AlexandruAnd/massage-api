97fef556f5d2c6a2e43bcb4da82752da1c6d1019
Alex <andreica.alexandru2000@gmail.com>
2020-05-24 14:10:34 +0300

    Initial commit (project structure)

---
+++ src/test/java/org/fasttrackit/massageapi/MassageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fasttrackit.massageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MassageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
