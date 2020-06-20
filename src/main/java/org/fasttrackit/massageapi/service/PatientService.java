95d62ed891814af27b885fccae3d234115a6495d
Alex <andreica.alexandru2000@gmail.com>
2020-06-20 20:08:33 +0300

    added PatientRepository and service

--- src/main/java/org/fasttrackit/massageapi/service/PatientService.java
+++ src/main/java/org/fasttrackit/massageapi/service/PatientService.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class PatientService {
     private static final Logger LOGGER = LoggerFactory.getLogger(PatientService.class);
