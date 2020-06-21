15e1af6e09a93d29a6ab770b17539e4453e1ab7f
Alex <andreica.alexandru2000@gmail.com>
2020-06-21 15:36:59 +0300

    added PatientController

---
+++ src/main/java/org/fasttrackit/massageapi/web/PatientController.java
@@ -0,0 +1,28 @@
+package org.fasttrackit.massageapi.web;
+
+import org.fasttrackit.massageapi.domain.Patient;
+import org.fasttrackit.massageapi.service.PatientService;
+import org.fasttrackit.massageapi.transfer.SavePatientRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+    private final PatientService patientService;
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Patient> createPatient(@Valid @RequestBody SavePatientRequest request){
+        Patient patient = patientService.createPatient(request);
+        return new ResponseEntity<>(patient, HttpStatus.CREATED);
+    }
+}
